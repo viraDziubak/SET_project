b30ac28a85beeb508d4da2cb4ce8b13cd2dcc7d7
viraDziubak <viradziubak806@gmail.com>
2020-04-30 01:13:14 +0300

    Initial commit Created Frameworks PageObject and Tests repositories. Added Selenium Settings and Wait frameworks. Added files BaseTest and PageObjectBase.

---
+++ SET_project/Frameworks/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SET_project.Frameworks
+{
+    public static class Settings
+    {
+        public static readonly string Url = TestContext.Parameters["URL"] ?? "http://automationpractice.com/index.php";
+        public static readonly Drivers Driver = (Drivers) Enum.Parse(typeof(Drivers),TestContext.Parameters["Driver"] ?? "Chrome");
+
+        public static readonly TimeSpan ImplicitWait = TimeSpan
+            .FromMilliseconds(Convert.ToInt32(TestContext.Parameters["WaitTime"]??"5000"));
+
+    }
+}
---
+++ SET_project/Tests/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using SET_project.Frameworks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace SET_project
+{
+    public abstract class BaseTest
+    {
+        protected readonly IWebDriver Driver;
+       
+        protected BaseTest()
+        {
+            Driver = Selenium.GetDriver(Settings.Driver);
+            Driver.Navigate().GoToUrl(Settings.Url);
+        }
+        static void Main ()
+        {
+            
+        }
+        
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => Driver.Quit();
+    }
+}
