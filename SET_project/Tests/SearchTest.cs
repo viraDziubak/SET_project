44531508b387d3eecd3334fff3a23fb2fc7da3bc
viraDziubak <viradziubak806@gmail.com>
2020-04-30 18:54:50 +0300

    Added correct searcing test

--- SET_project/Tests/SearchTest.cs
+++ SET_project/Tests/SearchTest.cs
@@ -1,7 +1,31 @@
-﻿namespace SET_project
+﻿using SET_project.PageObject;
+using NUnit.Framework;
+
+namespace SET_project.Tests
 {
-    public class SearchTest
+    [TestFixture]
+    public class SearchTest : BaseTest
     {
+        private Header _header;
+            
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _header = new Header(Driver);
+            
+        }
+
+        [TestCase(true,"mug")]
+        [TestCase(false,"qwe134")]
+        public void SearchCorrect(bool isCorrect, string searchString)
+        {
+            bool isSearchCorrect = _header.InputSerachText(searchString).SearchButtonClick().IsSearchOk();
+            
+            Assert.That(isSearchCorrect, Is.EqualTo(isCorrect), 
+                $"Searching works {(isSearchCorrect ? "successfully" : "unsuccessfully")}"+$", but we expected opposite");
+
+        } 
         
     }
+    
 }
