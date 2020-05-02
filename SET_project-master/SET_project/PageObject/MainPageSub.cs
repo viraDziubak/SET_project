52e5fa9fa6c2f188bd8a2076fda32c87ebe0be17
5toon <vitalii.vyniarskyi@gmail.com>
2020-05-03 02:42:23 +0300

    demo second test

---
+++ SET_project-master/SET_project/PageObject/MainPageSub.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace SET_project.PageObject
+{
+    public class MainPageSub : PageObjectBase
+    {
+        public MainPageSub(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private static readonly By EmailField = By.Name("email");
+
+        public MainPageSub ClickOnMailField()
+        {
+            Driver.FindElement(EmailField).Clear();
+            Driver.FindElement(EmailField).Click();
+            Driver.FindElement(EmailField).SendKeys(text);
+            
+        }
+    }
+}
---
+++ SET_project-master/SET_project/Tests/SubscribeTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using SET_project.PageObject;
+
+namespace SET_project.Tests
+{
+    public class SubscribeTest : BaseTest
+    {
+        private MainPageSub _mainPageTwo;
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _mainPageTwo = new MainPageSub(Driver);
+        
+    }
+}
