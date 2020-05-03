1ba62eaba0c63b6f899917c655db4531e080f221
5toon <vitalii.vyniarskyi@gmail.com>
2020-05-03 13:51:17 +0300

    second test

---
+++ SET_project-master/SET_project/Tests/ForgotPasswordTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SET_project.PageObject;
+
+namespace SET_project.Tests
+{
+    [TestFixture]
+    public class ForgotPasswordTest : BaseTest
+    {
+        private MainPage _mainPageTwo;
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => _mainPageTwo = new MainPage(Driver);
+
+        [TestCase(true, "test@gmail.com")]
+        [TestCase(false, "testmail")]
+        public void Test2(string email)
+        {
+            bool isEmailOk = _mainPageTwo.ClickToSignIn().ClickForgotButton().EnterMail(email).IsMailOk();
+            Assert.That(isEmailOk, Is.EqualTo(null), $"Email was validated {(isEmailOk ? "successfully" : "unsuccessfully")}"
+                                                     + " but were expected opposite");
+        }
+    }
+}
