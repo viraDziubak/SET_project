77a80d7a5ba2665c80e378def78bdf944714ba5c
5toon <vitalii.vyniarskyi@gmail.com>
2020-05-03 19:09:55 +0300

    full

---
+++ SET_project/Tests/ChangeLangTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using SET_project.PageObject;
+
+namespace SET_project.Tests
+{
+    [TestFixture]
+    public class ChangeLanguage : BaseTest
+    {
+        private Lang _header;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _header = new Lang(Driver);
+
+        }
+
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        
+        public void ChangeLang(bool isCorrect, bool isEngSelected)
+        {
+            bool isLangCorrect = _header.LangButtonClick(isEngSelected).IsLangChange(isEngSelected);
+            Assert.That(isLangCorrect, Is.EqualTo(isCorrect),
+                $"Language changed on {(isEngSelected ? "En" : "Uk")}"+$", but we expected opposite");
+                
+
+        }
+
+    }
+}
